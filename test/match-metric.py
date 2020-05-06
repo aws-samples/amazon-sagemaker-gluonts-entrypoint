1f3ad0346a0a844a605ed485f172e3760bad97ca
Verdi March <marcverd@amazon.com>
2020-05-06 15:01:25 +0800

    Update metric regexp with test:loss

--- test/match-metric.py
+++ test/match-metric.py
@@ -2,6 +2,7 @@ import re
 
 regex = [
     r"Epoch\[\d+\] Evaluation metric 'epoch_loss'=(\S+)",
+    r"Epoch\[\d+\] Evaluation metric 'validation_epoch_loss'=(\S+)",
     r"Epoch\[\d+\] Learning rate is (\S+)",
     r"gluonts\[metric-abs_error\]: (\S+)",
     r"gluonts\[metric-RMSE\]: (\S+)",
@@ -11,6 +12,7 @@ regex = [
 
 
 text = """[2020-04-23 09:26:06] [INFO] root Epoch[1] Evaluation metric 'epoch_loss'=6.790306
+[2020-05-06 14:52:09] [INFO] root Epoch[2] Evaluation metric 'validation_epoch_loss'=0.874746
 [2020-04-23 09:26:06] [INFO] root Epoch[2] Learning rate is 0.001
 [2020-04-23 09:26:10] [INFO] __main__ gluonts[metric-RMSE]: 47143.54635620117
 [2020-04-23 09:26:10] [INFO] __main__ gluonts[metric-abs_error]: 685.4429550170898
